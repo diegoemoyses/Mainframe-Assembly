378a64f716ced839dd88a05be99fa11535e43b5d
Diego Moyses <43712361+diegoemoyses@users.noreply.github.com>
2024-11-13 15:10:46 -0300

    Ajustes no código para melhor compreensão
    Ajustes no código para melhor compreensão 12/11/2024

--- ASMPLOOP.asm
+++ ASMPLOOP.asm
@@ -1,5 +1,6 @@
 *********************************************************************** 
 * OBJETIVOS: LOOP GRAVANDO O VALOR          06/11/2024                  
+*          : AJUSTE PARA MELHOR COMPREENSAO 12/11/2024                  
 *********************************************************************** 
 * PROCEDIMENTOS DE INICIALIZAÇÃO DO PROGRAMA (LINKAGE CONVENTION)       
 *********************************************************************** 
@@ -18,18 +19,21 @@ ASMPLOOP CSECT
 *********************************************************************** 
          OPEN  (SAIDA,OUTPUT)                                           
 *                                                                       
-         SR    R2,R2     * INICIA O REGISTRO QUE SERA O CONTADOR        
-         LA    R4,10     * CARREGA VALOR 10 NO R4 - LIMITE DO LOP       
-LOOP     EQU   *         * INICIO DO LOOP                               
-         LA    R2,1(R2)  * ADD 1 A R2                                   
+         LA    R2,10                                                    
+         PACK  NAP,NUMA                                                 
+         PACK  N1P,NUM01                                                
 *                                                                       
-         ST    R2,REG2                                                  
-         IC    0,MSG+L'MSG                                              
-         UNPK  MSG(5),REG2(5)                                           
-         NC    MSG,=8X'0F'                                              
-         TR    MSG,HEXTAB                                               
+*        LA    R3,0                                                     
+*        CVB   R4,N1P                                                   
+                                                                        
+*                                                                       
+LOOP     EQU   *                                                        
+         AP    NAP,N1P                                                  
+         MVC   MSGOUT,=X'202020'                                        
+         ED    MSGOUT,NAP                                               
+         MVI   MSGOUT+5,C'N'                                            
          PUT   SAIDA,MSG                                                
-         BCT   R4,LOOP   * FAZ O LOOP ATE ATINGIR VALOR DE R4           
+         BCT   R2,LOOP                                                  
 *                                                                       
          CLOSE SAIDA                                                    
 *********************************************************************** 
@@ -43,15 +47,19 @@ ASMPLOOP_MODULO_RETORNA_OK    DS  0H
 *********************************************************************** 
 * DEFINIÇÃO DE ÁREAS DE DADOS                                           
 *********************************************************************** 
-HEXTAB   DC    C'0123456789ABCDEF'                                      
 *                                                                       
-REG2     DS    F                                                        
+NUM01    DC    CL2'01'                                                  
+NUMA     DC    CL2'00'                                                  
 *                                                                       
-MSGALL   DS    0CL80                                                    
-MSG      DS    CL10                                                     
-         DC    20CL60' '                                                
+N1P      DS    PL2                                                      
+NAP      DS    PL2                                                      
 *                                                                       
-* AREA DE MEMORIA DCB APONTANDO PARA O DDNAME SAIDA   
+MSG      DS    0CL80                                                    
+         DC    CL1' '                                                   
+MSGOUT   DS    CL6                                                      
+         DC    59CL1' '                                                 
+*                                                                       
+* AREA DE MEMORIA DCB APONTANDO PARA O DDNAME SAIDA                     
 SAIDA    DCB   DDNAME=SAIDA,                                           X
                DSORG=PS,                                               X
                MACRF=PM,                                               X
@@ -69,14 +77,15 @@ R4       EQU   4
 R5       EQU   5                                                        
 R6       EQU   6                                                        
 R7       EQU   7                                                        
-R8       EQU   8                                                        
-R9       EQU   9                                                        
-R10      EQU  10                                                        
-R11      EQU  11                                                        
-R12      EQU  12                                                        
-R13      EQU  13                                                        
-R14      EQU  14                                                        
-R15      EQU  15                                                        
-*                                                                       
-         LTORG            
-         END		 
+R8       EQU   8 
+R9       EQU   9 
+R10      EQU  10 
+R11      EQU  11 
+R12      EQU  12 
+R13      EQU  13 
+R14      EQU  14 
+R15      EQU  15 
+*                
+         LTORG   
+*                
+         END     
